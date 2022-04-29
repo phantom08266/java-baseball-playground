51c64ccd34c90736592b80588d3c162775771648
phantom08266 <phantom08266@gmail.com>
2022-04-29 09:14:49 +0900

    코드 리펙터링
    한단계의 들여쓰기를 작성하도록 고민함.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,65 +1,67 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Calculator {
 
-    private ArrayList<String> marks = new ArrayList<>();
+    public static final String DEFAULT_CALCULATOR_MARK = "+";
+    private String[] marks = {"+", "-", "*", "/"};
 
     public int totalArgsCalculate(String[] args) {
-        boolean check = false;
-        int number = 0;
+
+        List<String> marks = Arrays.stream(args).filter(this::isMark)
+                .collect(Collectors.toList());
+
+        Queue<String> marksQueue = new LinkedList<>();
+        marksQueue.add(DEFAULT_CALCULATOR_MARK); // 코드 효율을 위한 +산술연산 추가 (이런 상황 및 주석 없는게 좋은데.. 더좋은 방법이 안떠오름.)
+        marksQueue.addAll(marks);
+
+        List<Integer> numbers = Arrays.stream(args).filter(arg -> !isMark(arg))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
         int total = 0;
-        for (String arg : args) {
-            if (isMark(arg)) {
-                check = true;
-                continue;
-            }
-
-            number = Integer.parseInt(arg);
-
-            if (check) {
-                total = calculate(total, number);
-                check = false;
-                continue;
-            }
-            total += number;
+        for (int num : numbers) {
+            String mark = marksQueue.poll();
+            total = calculate(total, num, mark);
         }
 
         return total;
     }
 
-    private int calculate(int lValue, int rValue) {
-        if (marks.isEmpty()) return 0;
+    private int calculate(int lValue, int rValue, String mark) {
+        if (mark == null) return 0;
 
-        String mark = marks.get(0);
-        marks.clear();
         switch (mark) {
             case "+":
-                return lValue + rValue;
+                return add(lValue, rValue);
             case "-":
-                return lValue - rValue;
+                return sub(lValue, rValue);
             case "*":
-                return lValue * rValue;
+                return multiple(lValue, rValue);
             case "/":
-                return lValue / rValue;
+                return division(lValue, rValue);
         }
         return 0;
     }
 
     private boolean isMark(String arg) {
-        switch (arg) {
-            case "+":
-                marks.add("+");
-                return true;
-            case "-":
-                marks.add("-");
-                return true;
-            case "*":
-                marks.add("*");
-                return true;
-            case "/":
-                marks.add("/");
-                return true;
-        }
-        return false;
+        return Arrays.asList(marks).contains(arg);
+    }
+
+    private int add(int lValue, int rValue) {
+        return lValue + rValue;
+    }
+
+    private int sub(int lValue, int rValue) {
+        return lValue - rValue;
+    }
+
+    private int multiple(int lValue, int rValue) {
+        return lValue * rValue;
+    }
+
+    private int division(int lValue, int rValue) {
+        return lValue / rValue;
     }
 }
