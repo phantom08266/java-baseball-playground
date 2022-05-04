a6b442a7423204739daf9c44d5900f0376b308b9
phantom08266 <phantom08266@gmail.com>
2022-05-04 09:58:26 +0900

    Feat 사용자 입력값 검증 부분 기능 추가

--- src/main/java/baseball/BaseBallGameMachine.java
+++ src/main/java/baseball/BaseBallGameMachine.java
@@ -1,8 +1,47 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class BaseBallGameMachine {
 
+    private BaseBallNumberMaker baseBallNumberMaker;
+
+    public BaseBallGameMachine(BaseBallNumberMaker baseBallNumberMaker) {
+        this.baseBallNumberMaker = baseBallNumberMaker;
+    }
+
     public boolean validation(String userInputGameNumber) {
-        return true;
+        String randomGameNumber = baseBallNumberMaker.createRandomGameNumber();
+        System.out.println("컴퓨터 숫자 : "+ randomGameNumber);
+        if (randomGameNumber.equals(userInputGameNumber)) return true;
+
+        checkBallGameNumber(userInputGameNumber, randomGameNumber);
+        checkStrikeGameNumber(userInputGameNumber, randomGameNumber);
+        System.out.println();
+
+        return false;
+    }
+
+    private void checkStrikeGameNumber(String userNumber, String machineNumber) {
+        char[] userNumbers = userNumber.toCharArray();
+        char[] machineNumbers = machineNumber.toCharArray();
+        int loopCount = userNumber.length();
+
+        long strikeCount = Stream.iterate(0, i -> i + 1).limit(loopCount)
+                .filter(i -> userNumbers[i] == machineNumbers[i])
+                .count();
+
+        System.out.print(String.format("%d스트라이크", strikeCount));
+    }
+
+    private void checkBallGameNumber(String userNumber, String machineNumber) {
+
+        long ballCount = userNumber.chars()
+                .mapToObj(c -> (char) c)
+                .filter(item -> machineNumber.contains(item.toString()))
+                .count();
+
+        System.out.print(String.format("%d볼 ", ballCount));
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,14 @@
 import baseball.BaseBallGameMachine;
 import baseball.BaseBallGameViewer;
+import baseball.BaseBallNumberMaker;
 import baseball.UserGameNumberController;
 
 public class main {
     public static void main(String[] args) {
-        BaseBallGameMachine baseBallGameMachine = new BaseBallGameMachine();
+        // 객체들간의 의존성 연결
+        // 리펙토링 책에 나오는 객체들 간의 초기화 부분!
+        BaseBallNumberMaker baseBallNumberMaker = new BaseBallNumberMaker();
+        BaseBallGameMachine baseBallGameMachine = new BaseBallGameMachine(baseBallNumberMaker);
         UserGameNumberController userGameNumberController = new UserGameNumberController();
         BaseBallGameViewer baseBallGameViewer = new BaseBallGameViewer(baseBallGameMachine, userGameNumberController);
 
