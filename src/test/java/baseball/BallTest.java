5d389837f6b9925151911b2764b2c76da2ecbada
phantom08266 <phantom08266@gmail.com>
2022-05-08 21:24:56 +0900

    Refactor 테스트케이스 리팩토링

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -2,15 +2,22 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BallTest {
 
+    private Ball computerBall;
+
+    @BeforeEach
+    void setup() {
+        computerBall = new Ball(1, new GameBall(3));
+    }
+
     @Test
     @DisplayName("게임 값 포함, 위치가 일치하면 1 Strike")
     void test1() {
-        Ball computerBall = new Ball(1, new GameBall(3));
         Ball userBall = new Ball(1, new GameBall(3));
 
         assertThat(computerBall.play(userBall)).isEqualTo(GameStatus.STRIKE);
@@ -20,7 +27,6 @@ public class BallTest {
     @Test
     @DisplayName("게임 값은 포함, 위치 불일치 하면 1 ball")
     void test2() {
-        Ball computerBall = new Ball(1, new GameBall(3));
         Ball userBall = new Ball(2, new GameBall(3));
 
         assertThat(computerBall.play(userBall)).isEqualTo(GameStatus.BALL);
@@ -29,7 +35,6 @@ public class BallTest {
     @Test
     @DisplayName("게임 값 포함안함, 위치 불일치 하면 noting")
     void test3() {
-        Ball computerBall = new Ball(1, new GameBall(3));
         Ball userBall = new Ball(2, new GameBall(4));
 
         assertThat(computerBall.play(userBall)).isEqualTo(GameStatus.NOTING);
